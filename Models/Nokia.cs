0244baea6af8b4b042d8c15c56afc54fc3cb64c6
jose celestino <jose8.trabalho@protonmail.com>
2023-12-19 10:12:44 -0300

    Testes realizados

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -2,17 +2,15 @@ using System.Security.Cryptography;
 
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
     public class Nokia : Smartphone
     {
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
 
         }
-        // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
         {
-            Console.WriteLine($"Instalando aplicativo {nomeApp}no Nokia");
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Nokia");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("44441123", "G11 Plus", "2222222222", 4);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Tetris");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("89897327", "Iphone 15", "11223344", 16);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Subway Surfers");
